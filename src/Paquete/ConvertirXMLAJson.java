ec9e6d2ad697e14242ecd361f2fe837d259402e4
MelissaRodriguezHernandez <mrodriguezhernandez@cifpfbmoll.eu>
2022-10-17 17:03:26 +0200

    convertir XML a Json

---
+++ src/Paquete/ConvertirXMLAJson.java
@@ -0,0 +1,18 @@
+package Paquete;
+
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ConvertirXMLAJson {
+
+    public static final String rutaFichero = "C:\\Users\\Mely\\IdeaProjects\\Matricula\\src\\Paquete\\CicleModulos.xml";
+
+    public static void main(String[] args) throws IOException {
+
+        String strfitxerXML = new String(Files.readAllBytes(Paths.get(rutaFichero)));
+        System.out.println(strfitxerXML);
+
+    }
+}
